4ebe949d3d3b16f12d6e190c726ae13d052c7892
Aswathi Nambiar <aswathim@buffalo.edu>
2018-03-25 18:35:45 -0700

    Add files via upload

--- Assignment1-Logistic Regression.R
+++ Assignment1-Logistic Regression.R
@@ -16,11 +16,11 @@ march_01_df = read.csv("March_01.csv")
 march_02_df = read.csv("March_02.csv")
 
 
-blog_data['Outcome'] <- ifelse(blog_data$X1.0.2 > mean(blog_data$X1.0.2) ,1,0)
-feb_01_df['Outcome'] <- ifelse(feb_01_df$X4.0 > mean(feb_01_df$X4.0),1,0)
-feb_02_df['Outcome'] <- ifelse(feb_02_df$X1.0.23 > mean(feb_02_df$X1.0.23),1,0)
-march_01_df['Outcome'] <- ifelse(march_01_df$X0.0.239 > mean(march_01_df$X0.0.239),1,0)
-march_02_df['Outcome'] <- ifelse(march_02_df$X1.0.15 >mean(march_02_df$X1.0.15),1,0)
+blog_data['Outcome'] <- ifelse(blog_data$X1.0.2 > 0 ,1,0)
+feb_01_df['Outcome'] <- ifelse(feb_01_df$X4.0 > 0,1,0)
+feb_02_df['Outcome'] <- ifelse(feb_02_df$X1.0.23 > 0,1,0)
+march_01_df['Outcome'] <- ifelse(march_01_df$X0.0.239 > 0,1,0)
+march_02_df['Outcome'] <- ifelse(march_02_df$X1.0.15 > 0 ,1,0)
 
 
 #Logistic Regression for basic features
@@ -69,6 +69,43 @@ log_t_feb_02_test <- textual_column_change(log_t_feb_02_test)
 log_t_march_01_test <- textual_column_change(log_t_march_01_test)
 log_t_march_02_test <- textual_column_change(log_t_march_02_test)
 
+#Applying PCA
+library(caret)
+#install.packages('e1071', dependencies=TRUE)
+library(e1071)
+#------------Applying PCA for Basic Features ------------------------------
+pca = preProcess(x = log_basic_df[-11],method='pca',pcaComp = 2)
+log_basic_df <- predict(pca,log_basic_df)
+log_basic_df <- log_basic_df[c(2,3,1)]
+
+log_b_feb_01_test <- predict(pca,log_b_feb_01_test)
+log_b_feb_01_test <- log_b_feb_01_test[c(2,3,1)]
+
+log_b_feb_02_test <- predict(pca,log_b_feb_02_test)
+log_b_feb_02_test <- log_b_feb_02_test[c(2,3,1)]
+
+log_b_march_01_test <- predict(pca,log_b_march_01_test)
+log_b_march_01_test <- log_b_march_01_test[c(2,3,1)]
+
+log_b_march_02_test <- predict(pca,log_b_march_02_test)
+log_b_march_02_test <- log_b_march_02_test[c(2,3,1)]
+
+#------------Applying PCA for Textual Features ------------------------------
+pca_1 = preProcess(x = log_textual_df[-201],method='pca',pcaComp = 2)
+log_textual_df <- predict(pca_1,log_textual_df)
+log_textual_df <- log_textual_df[c(2,3,1)]
+
+log_t_feb_01_test <- predict(pca_1,log_t_feb_01_test)
+log_t_feb_01_test <- log_t_feb_01_test[c(2,3,1)]
+
+log_t_feb_02_test <- predict(pca_1,log_t_feb_02_test)
+log_t_feb_02_test <- log_t_feb_02_test[c(2,3,1)]
+
+log_t_march_01_test <- predict(pca_1,log_t_march_01_test)
+log_t_march_01_test <- log_t_march_01_test[c(2,3,1)]
+
+log_t_march_02_test <- predict(pca_1,log_t_march_02_test)
+log_t_march_02_test <- log_t_march_02_test[c(2,3,1)]
 
 #------------------Logistic Regression for Basic Features----------------------------
 
@@ -76,25 +113,21 @@ logregressor = glm(formula = Btarget ~.,family = binomial(link='logit'),
                    data = log_basic_df)
 summary(logregressor)
 
-logregressor_1 = glm(formula = Btarget ~ B51+B52+B54,family = binomial(link ='logit'),
-                     data = log_basic_df)
-summary(logregressor_1)
 
-log_b_predict_probfeb01 = predict(logregressor_1,type= 'response',newdata = log_b_feb_01_test)
-log_b_predict_probfeb02 = predict(logregressor_1,type= 'response',newdata = log_b_feb_02_test)
-log_b_predict_probmar01 = predict(logregressor_1,type= 'response',newdata = log_b_march_01_test)
-log_b_predict_probmar02 = predict(logregressor_1,type= 'response',newdata = log_b_march_02_test)
+log_b_predict_probfeb01 = predict(logregressor,type= 'response',newdata = log_b_feb_01_test[-3])
+log_b_predict_probfeb02 = predict(logregressor,type= 'response',newdata = log_b_feb_02_test[-3])
+log_b_predict_probmar01 = predict(logregressor,type= 'response',newdata = log_b_march_01_test[-3])
+log_b_predict_probmar02 = predict(logregressor,type= 'response',newdata = log_b_march_02_test[-3])
 
-print(log_b_predict_probfeb01)
 
-log_b_pred_feb_01 = ifelse(log_b_predict_probfeb01 > 0.8,1,0)
-log_b_pred_feb_02 = ifelse(log_b_predict_probfeb02 > 0.8,1,0)
-log_b_pred_mar_01 = ifelse(log_b_predict_probmar01 > 0.8,1,0)
-log_b_pred_mar_02 = ifelse(log_b_predict_probmar02 > 0.8,1,0)
+log_b_pred_feb_01 = ifelse(log_b_predict_probfeb01 > 0.5,1,0)
+log_b_pred_feb_02 = ifelse(log_b_predict_probfeb02 > 0.5,1,0)
+log_b_pred_mar_01 = ifelse(log_b_predict_probmar01 > 0.5,1,0)
+log_b_pred_mar_02 = ifelse(log_b_predict_probmar02 > 0.5,1,0)
 
 #install.packages("caret")
 #install.packages('e1071', dependencies=TRUE)
-library(caret)
+
 cm_feb01 = confusionMatrix(log_b_pred_feb_01,log_b_feb_01_test$Btarget)
 cm_feb02 = confusionMatrix(log_b_pred_feb_02,log_b_feb_02_test$Btarget)
 cm_mar01 = confusionMatrix(log_b_pred_mar_01,log_b_march_01_test$Btarget)
@@ -103,7 +136,7 @@ cm_mar02 = confusionMatrix(log_b_pred_mar_02,log_b_march_02_test$Btarget)
 print(cm_feb01)
 print(cm_feb02)
 print(cm_mar01)
-print(cm_mar01)
+print(cm_mar02)
 
 #------------------Logistic Regression for Textual Features----------------------------
 
@@ -111,32 +144,26 @@ logregressor = glm(formula = Ttarget ~.,family = binomial(link='logit'),
                    data = log_textual_df)
 summary(logregressor)
 
-logregressor_1 = glm(formula = Ttarget ~ T67+T72+T77+T92+T104+
-                      T119+T128+T134+T135+T137+T138+T153+T164+T227+T241,
+logregressor_1 = glm(formula = Ttarget ~ PC2,
                      family = binomial(link ='logit'),
                      data = log_textual_df)
 summary(logregressor_1)
 
-logregressor_2 = glm(formula = Ttarget ~ T67+T72+T77+T104+
-                       T119+T128+T134+T135+T137+T164+T227+T241,
-                     family = binomial(link ='logit'),
-                     data = log_textual_df)
-summary(logregressor_2)
 
-log_t_predict_probfeb01 = predict(logregressor_2,type= 'response',newdata = log_t_feb_01_test)
-log_t_predict_probfeb02 = predict(logregressor_2,type= 'response',newdata = log_t_feb_02_test)
-log_t_predict_probmar01 = predict(logregressor_2,type= 'response',newdata = log_t_march_01_test)
-log_t_predict_probmar02 = predict(logregressor_2,type= 'response',newdata = log_t_march_02_test)
+log_t_predict_probfeb01 = predict(logregressor_1,type= 'response',newdata = log_t_feb_01_test[-3])
+log_t_predict_probfeb02 = predict(logregressor_1,type= 'response',newdata = log_t_feb_02_test[-3])
+log_t_predict_probmar01 = predict(logregressor_1,type= 'response',newdata = log_t_march_01_test[-3])
+log_t_predict_probmar02 = predict(logregressor_1,type= 'response',newdata = log_t_march_02_test[-3])
 
 print(log_t_predict_probfeb01)
 
-log_t_pred_feb_01 = ifelse(log_t_predict_probfeb01 > 0.8,1,0)
-log_t_pred_feb_02 = ifelse(log_t_predict_probfeb02 > 0.8,1,0)
-log_t_pred_mar_01 = ifelse(log_t_predict_probmar01 > 0.8,1,0)
-log_t_pred_mar_02 = ifelse(log_t_predict_probmar02 > 0.8,1,0)
+log_t_pred_feb_01 = ifelse(log_t_predict_probfeb01 > 0.5,1,0)
+log_t_pred_feb_02 = ifelse(log_t_predict_probfeb02 > 0.5,1,0)
+log_t_pred_mar_01 = ifelse(log_t_predict_probmar01 > 0.5,1,0)
+log_t_pred_mar_02 = ifelse(log_t_predict_probmar02 > 0.5,1,0)
 
 #install.packages("caret")
-#install.packages('e1071', dependencies=TRUE)
+
 library(caret)
 cm_feb01 = confusionMatrix(log_t_pred_feb_01,log_t_feb_01_test$Ttarget)
 cm_feb02 = confusionMatrix(log_t_pred_feb_02,log_t_feb_02_test$Ttarget)
@@ -149,3 +176,5 @@ print(cm_mar01)
 print(cm_mar02)
 
 #--------------------------------END-----------------------------------------------
+
+
